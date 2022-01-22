edcbc1ef29b3e857a485c9f5ef2eecd950bb1966
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2022-01-22 14:53:00 -0800

    chore: update discord link

--- Common/src/main/kotlin/com/github/redditvanced/common/Constants.kt
+++ Common/src/main/kotlin/com/github/redditvanced/common/Constants.kt
@@ -12,6 +12,11 @@ object Constants {
 	 */
 	const val GITHUB = "https://github.com/$PROJECT_NAME/$PROJECT_NAME"
 
+	/**
+	 * Discord support server
+	 */
+	const val DISCORD = "https://discord.gg/yZGanTPdPF"
+
 	val APP_VERSION_CODE: Int = FrontpageSettings.i.appVersionCode
 	val APP_VERSION_NAME: String = FrontpageSettings.i.appVersionName
 
@@ -34,17 +39,5 @@ object Constants {
 		val CORE_SETTINGS = File("$BASE/settings.json")
 		val CUSTOM_CORE = File("$BASE/build/core.zip")
 		val INJECTOR_DEX = File("$BASE/build/injector.dex")
-	}
-
-	object Discord {
-		// TODO: update server info
-		const val SERVER_ID = 0L
-		const val INVITE = ""
-
-		object Channels {
-			const val GENERAL = 0L
-			const val THEME_DEV = 0L
-			const val PLUGIN_DEV = 0L
-		}
 	}
 }
