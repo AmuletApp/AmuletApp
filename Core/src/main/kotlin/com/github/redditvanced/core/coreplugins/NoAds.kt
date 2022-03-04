5a850b443603a7beeb9700023059335b6f7c1f2f
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2022-03-03 16:58:21 -0800

    feat: NoAds

--- Core/src/main/kotlin/com/github/redditvanced/core/coreplugins/NoAds.kt
+++ Core/src/main/kotlin/com/github/redditvanced/core/coreplugins/NoAds.kt
@@ -1,7 +1,38 @@
 package com.github.redditvanced.core.coreplugins
 
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import com.reddit.domain.model.ILink
+import com.reddit.domain.model.listing.Listing
+import com.reddit.frontpage.presentation.detail.CommentScreenAdView
+
 class NoAds : CorePlugin("NoAds", "Remove advertisements and promoted posts from your feed.") {
 	override fun onStart() {
-		// TODO: No ads or promoted posts patches
+		val cString = String::class.java
+
+		// Ads under opened posts
+		patcher.after<CommentScreenAdView>(Context::class.java, AttributeSet::class.java) {
+			rootView.visibility = View.GONE
+		}
+
+		// Ads in feed
+		patcher.before<Listing<*>>(
+			List::class.java,
+			cString,
+			cString,
+			cString,
+			cString,
+			Boolean::class.javaPrimitiveType!!
+		) {
+			val list = it.args[0] as List<*>
+
+			if (list[0] !is ILink)
+				return@before
+
+			it.args[0] = list.filterNot { link ->
+				(link as ILink).promoted
+			}
+		}
 	}
 }
