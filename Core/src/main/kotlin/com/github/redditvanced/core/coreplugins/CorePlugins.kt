51773d268754d11b6fea5abe6df454ef0f9521b5
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2022-03-31 16:55:23 -0700

    fix(CorePlugins): Aliucord -> Amulet

--- Core/src/main/kotlin/com/github/redditvanced/core/coreplugins/CorePlugins.kt
+++ Core/src/main/kotlin/com/github/redditvanced/core/coreplugins/CorePlugins.kt
@@ -1,6 +1,7 @@
 package com.github.redditvanced.core.coreplugins
 
 import com.github.redditvanced.common.models.PluginManifest
+import com.github.redditvanced.common.models.PluginManifest.Author
 import com.github.redditvanced.core.entities.Plugin
 
 object CorePlugins {
@@ -20,7 +21,7 @@ open class CorePlugin(name: String, description: String) : Plugin(
 	PluginManifest(
 		name,
 		description,
-		listOf(PluginManifest.Author("Aliucord", null, null)),
+		listOf(Author("Amulet", null, null)),
 		loadResources = false,
 		requiresRestart = false,
 		pluginClass = "",
